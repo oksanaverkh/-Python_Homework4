20736c2e1a6b8c9b024aca227b1ed92bec6fb985
oksanaverkh <oksana-tyryatkina@yandex.ru>
2022-12-24 18:11:51 +0300

    Next commit

--- Task3.py
+++ Task3.py
@@ -1,4 +1,30 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-если есть дубли у элемента, то его не выводить вообще
+import os
+import random
+import math
+os.system('cls')
+
+def list_creation(n, limit):
+    new_list = []
+    for _ in range(n):
+        new_list.append(random.randint(-limit, limit))
+    return new_list
+
+def unique_list(new_list):
+        non_repeat_list = []
+        for i in range(len(new_list)):
+            if new_list.count(new_list[i])==1:
+                non_repeat_list.append(new_list[i])
+        return non_repeat_list
+
+def main():    
+    n = abs(int(input('Enter list length: ')))
+    limit = abs(int(input('Enter list limit value: ')))
+    my_list = list_creation(n, limit)
+    print(my_list)
+    print(unique_list(my_list))
+
+if __name__ == "__main__":
+    main()
