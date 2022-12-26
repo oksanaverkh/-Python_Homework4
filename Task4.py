9b9f67687a1cc55cd7a042b6b5738718cfbb8927
oksanaverkh <oksana-tyryatkina@yandex.ru>
2022-12-26 17:37:57 +0300

    Next commit

--- Task4.py
+++ Task4.py
@@ -8,21 +8,36 @@ import os
 import random
 os.system('cls')
 
-k = int(input('Enter degree K: '))
+def list_creation(num, limit):
+    new_list = []
+    for _ in range(num+1):
+        new_list.append(random.randint(0, 100))
+    print('List of coefficients:', new_list)
+    return new_list
 
-my_list = []
-for _ in range(k+1):
-    my_list.append(random.randint(0, 101))
-print('List of coefficients:', my_list)
+def check():
+    k = int(input('Enter degree K: '))
+    while not 0<=k<=100:
+        print('Wrong value, try again')
+        k = int(input('Enter degree K: '))
+    return int(k)
 
-with open('polynomial.txt', 'w') as data:
-    for i in range(k+1):
-        if my_list[i]==0:
-            continue
-        elif (k-i)==1:
-            data.write(f' {my_list[i]*1}x +')
-        elif (k-i)==0:
-            data.write(f' {my_list[i]*1}')
-        else:
-            data.write(f' {my_list[i]}x^{k-i} +')
-    data.write(' = 0')
+def main():
+    k = check()    
+    my_list = list_creation(k, 100)
+    with open('polynomial.txt', 'w') as data:
+        for i in range(k+1):
+            if my_list[i]==0:
+                continue
+            elif my_list[i]==1:
+                data.write(f' x^{k-i} +')
+            elif (k-i)==1:
+                data.write(f' {my_list[i]*1}x +')
+            elif (k-i)==0:
+                data.write(f' {my_list[i]*1}')
+            else:
+                data.write(f' {my_list[i]}x^{k-i} +')
+        data.write(' = 0')
+
+if __name__ == "__main__":
+    main()
